12e2d3e2c2f05723a1dc0124918dfddfc101fad5
evgeniy <eugeniymalakhov@gmail.com>
2024-10-08 14:57:07 +0300

    min heap implementation added

---
+++ Heap Algorithms/min-heap.js
@@ -0,0 +1,95 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    getParentIndex(idx) {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    getLeftChildIndex(idx) {
+        return 2 * idx + 1;
+    }
+
+    getRightChildIndex(idx) {
+        return 2 * idx + 2;
+    }
+
+    swap(idx1, idx2) {
+        [this.heap[idx1], this.heap[idx2]] = [this.heap[idx2], this.heap[idx1]];
+    }
+
+    insert(value) {
+        this.heap.push(value);
+        this.bubbleUp()
+    }
+
+    bubbleUp() {
+        let idx = this.heap[this.heap.length - 1];
+        while (idx > 0 && this.heap[this.getParentIndex(idx)] > this.heap[idx]) {
+            this.swap(idx, this.getParentIndex(idx));
+            idx = this.getParentIndex(idx);
+        }
+    }
+
+    remove() {
+        if (this.isEmpty()) {
+            return null
+        }
+
+        if (this.heap.length === 1) return this.heap.pop()
+
+        const root = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.sinkDown();
+        return root;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        while (this.getLeftChildIndex(idx) < this.heap.length) {
+            let leftChildIdx = this.getLeftChildIndex(idx);
+            let rightChildIdx = this.getRightChildIndex(idx);
+
+            if (rightChildIdx < this.heap.length && this.heap[rightChildIdx] < this.heap[leftChildIdx]) {
+                leftChildIdx = rightChildIdx;
+            }
+
+            if (this.heap[idx] <= this.heap[leftChildIdx]) break;
+
+            this.swap(idx, leftChildIdx);
+            idx = leftChildIdx
+        }
+    }
+
+    isEmpty() {
+        return !this.heap.length
+    }
+}
+
+const heap = new MinHeap();
+heap.insert(2);
+heap.insert(4);
+heap.insert(1);
+heap.insert(6);
+heap.insert(3);
+heap.insert(5);
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
+
+console.log(heap.remove());
+console.log(heap.heap)
